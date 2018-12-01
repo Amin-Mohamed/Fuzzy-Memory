159eb23c7eb101165b5c6ed7eafbbb6fbf985f03
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-12-01 01:06:20 +0100

    match function fixed

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 'use strict';
 
-// shuffle images and show the game when replay button is clicked
 const startButton = document.getElementById('start-button')
 const game = document.getElementById('game')
+const title = document.getElementById('splash-screen')
+const replayButton = document.getElementById('button-div')
 
+// shuffle images and show the game when start button is clicked
 startButton.addEventListener('click', function (e) {
   cards.forEach(card => {
     let RandomImg = Math.floor(Math.random() * 16)
@@ -12,9 +14,12 @@ startButton.addEventListener('click', function (e) {
   })
   startButton.style.visibility='hidden'
   game.style.visibility='visible'
+  title.style.visibility='hidden'
+  replayButton.style.visibility='visible'
 })
 
-// shuffle images when replay button clicked
+
+// shuffle cards when replay button is clicked
 let replay = document.querySelector('.button')
   replay.addEventListener('click', function(e){
     cards.forEach(card => {
@@ -23,32 +28,71 @@ let replay = document.querySelector('.button')
     })
   });
 
-//flip the cards
-let hasFlipped = false;
-let firstCard, secondCard;
-
 let cards = document.querySelectorAll('.card')
-cards.forEach(card => card.addEventListener('click', function(e){
-  this.classList.toggle('flip')
+cards.forEach(card => card.addEventListener('click', flipCard))
 
+
+let hasFlipped = false
+let firstCard, secondCard
+
+//flip the cards
+function flipCard() {
+  this.classList.add('flip')
+  //firstCard
   if (!hasFlipped) {
     hasFlipped = true
     firstCard = this
-    // console.log(hasFlipped, firstCard);
+    return
   }
-    else {
-      hasFlipped = true
+      //secandCard
+      hasFlipped = false
       secondCard = this
-    }
-    // console.log(firstCard, secondCard);
-    if (secondCard) {
+      matchCheck()
+}
+// card match check
+function matchCheck() {
 
-      if (firstCard.dataset.img === secondCard.dataset.img){
-        console.log('works')
-      }
-    }
+  if (firstCard.dataset.img === secondCard.dataset.img)
+  {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+      console.log('works');
+  }
+  
+  else
+  {
+  setTimeout(() =>{
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+  }, 1000);
+}
+}
 
-  }));
+
+
+
+
+// cards.forEach(card => card.addEventListener('click', function(e){
+//   this.classList.toggle('flip')
+//
+//   if (!hasFlipped) {
+//     hasFlipped = true
+//     firstCard = this
+//     // console.log(hasFlipped, firstCard);
+//   }
+//     else {
+//       hasFlipped = true
+//       secandCard = this
+//     }
+//     // console.log(firstCard, secondCard);
+//     if (secandCard) {
+//       if (firstCard.dataset.img === secandCard.dataset.img){
+//         console.log('works')
+//       }
+//     }
+//
+//   }));
+
 
   // function flipCard() {
   //   this.classList.toggle('flip');
